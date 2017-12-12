5a815ab9ae6cd3fd7144e12fdd37fc5182118ade
Teocci Kseek-PC-Ubuntu <teocci@yandex.com>
2017-12-12 11:29:27 +0900

    fixing test006

--- test006.cmd
+++ test006.cmd
@@ -6,7 +6,7 @@ setlocal ENABLEEXTENSIONS
 set KEY_NAME="HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\VideoLAN\VLC"
 set VALUE_NAME=InstallDir
 
-FOR /F "tokens=2*" %%A IN ('REG query "%KEY_NAME%" /v "%VALUE_NAME%"') DO (set pInstallDir=%%B)
+FOR /F "skip=2 tokens=2*" %%A IN ('REG query "%KEY_NAME%" /v "%VALUE_NAME%"') DO (set pInstallDir=%%B)
 
 if defined pInstallDir (
 	echo %pInstallDir%
