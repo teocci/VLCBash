aac90e3941290a21d4d140f25dd6fb84e67e3fdd
Teocci Kseek-PC-Ubuntu <teocci@yandex.com>
2017-12-12 11:42:28 +0900

    fixing test3

--- test3.cmd
+++ test3.cmd
@@ -5,24 +5,24 @@ setlocal ENABLEEXTENSIONS
 set KEY_NAME="HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\VideoLAN\VLC"
 set VALUE_NAME=InstallDir
 
-for /f "usebackq tokens=3*" %%a in (`reg query "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\VideoLAN\VLC" /s`) do (
+for /f "usebackq tokens=1-2*" %%a in (`reg query "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\VideoLAN\VLC" /s`) do (
     set ValueName=%%a
     set ValueType=%%b
     set ValueValue=%%c %%d
 
-  set _vlc_path=%%a %%b
-  echo !_vlc_path!
+    set _vlc_path=%%c %%d
+    echo !_vlc_path!
 
-if defined ValueName (
-    @echo Value Name = %ValueName%
-    @echo Value Type = %ValueType%
-    @echo Value Value = %ValueValue%
+    if defined ValueName (
+        @echo Value Name = %ValueName%
+        @echo Value Type = %ValueType%
+        @echo Value Value = %ValueValue%
 
-    @echo Value Name = !ValueName!
-    @echo Value Type = !ValueType!
-    @echo Value Value = !ValueValue!
-) else (
-    @echo "%KEY_NAME:"=%\%VALUE_NAME%" not found.
-)
+        @echo Value Name = !ValueName!
+        @echo Value Type = !ValueType!
+        @echo Value Value = !ValueValue!
+    ) else (
+        @echo "%KEY_NAME:"=%\%VALUE_NAME%" not found.
+    )
 )
 endlocal
