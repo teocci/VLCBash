490bd36dfb96ef830bd0014fb21af1ea0535eb1c
Teocci Kseek-PC-Ubuntu <teocci@yandex.com>
2017-12-12 12:03:14 +0900

    fixing test007 path

--- test007.cmd
+++ test007.cmd
@@ -4,6 +4,7 @@ setlocal enabledelayedexpansion
 setlocal ENABLEEXTENSIONS
 set KEY_NAME="HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\VideoLAN\VLC"
 set VALUE_NAME=InstallDir
+set USER_PATH="%path%"
 
 for /f "usebackq tokens=1-2*" %%a in (`reg query "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\VideoLAN\VLC" /s`) do (
     set ValueName=%%a
@@ -15,6 +16,7 @@ for /f "usebackq tokens=1-2*" %%a in (`reg query "HKEY_LOCAL_MACHINE\SOFTWARE\WO
 	        @echo Value Name = !ValueName!
 	        @echo Value Type = !ValueType!
 	        @echo Value Value = !ValueValue!
+	        if not x%USER_PATH:ValueValue=%==x%USER_PATH% echo It contains !ValueValue! 
 		)
     ) else (
         @echo "%KEY_NAME:"=%\%VALUE_NAME%" not found.
