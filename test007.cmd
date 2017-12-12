91b5f19f9b346530268857a327b8944dfb740841
Teocci Kseek-PC-Ubuntu <teocci@yandex.com>
2017-12-12 11:50:44 +0900

    fixing test007

--- test007.cmd
+++ test007.cmd
@@ -1,20 +1,23 @@
-@echo OFF
-
+@echo off
+setlocal
+setlocal enabledelayedexpansion
 setlocal ENABLEEXTENSIONS
 set KEY_NAME="HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\VideoLAN\VLC"
 set VALUE_NAME=InstallDir
 
-FOR /F "usebackq skip=2 tokens=1-2*" %%A IN (`REG QUERY %KEY_NAME% /v %VALUE_NAME% 2^>nul`) DO (
-    set ValueName=%%A
-    set ValueType=%%B
-    set ValueValue=%%C
-)
+for /f "usebackq tokens=1-2*" %%a in (`reg query "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\VideoLAN\VLC" /s`) do (
+    set ValueName=%%a
+    set ValueType=%%b
+    set ValueValue=%%c
 
-if defined ValueName (
-    @echo Value Name = %ValueName%
-    @echo Value Type = %ValueType%
-    @echo Value Value = %ValueValue%
-) else (
-    @echo %KEY_NAME%\%VALUE_NAME% not found.
+    if defined ValueName (
+    	if !ValueName! == !VALUE_NAME! (
+	        @echo Value Name = !ValueName!
+	        @echo Value Type = !ValueType!
+	        @echo Value Value = !ValueValue!
+		)
+    ) else (
+        @echo "%KEY_NAME:"=%\%VALUE_NAME%" not found.
+    )
 )
 endlocal
